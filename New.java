51a6e15b103aaa0fe26f75930f8f3f6a16d6da5a
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2025-05-05 09:22:34 +0100

    update

--- New.java
+++ New.java
@@ -2,5 +2,12 @@ public class New{
     private String title;
     private String author;
     private Double price;
+
+    public New (String title, String author, Double Price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    public static void 
 }
     
